e7de960404f403e503e9ab16aa5bc9d2f36a3e2e
Himanshu Sharma <144505434+himanshujangid03@users.noreply.github.com>
2024-07-05 07:19:44 +0530

    Update GoogleAuth.jsx

--- src/features/authentication/google auth/GoogleAuth.jsx
+++ src/features/authentication/google auth/GoogleAuth.jsx
@@ -7,6 +7,7 @@ function GoogleAuth() {
       <Link
         to={"https://noteworthy-server-latest.onrender.com/auth/google"}
         data-tip="implement soon..."
+        disabled={true}
         className="grid grid-cols-[auto,1fr] w-full h-10 lg:h-14 hover:bg-blue-500 shadow-xl shadow-[primary]/10 items-center p-1 btn bg-blue-600 text-white tooltip"
       >
         <FcGoogle className="size-6 bg-white h-full w-10 lg:w-12 px-2 rounded-lg" />
